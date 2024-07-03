48c5723317821d292c811db7ac1434129933ab6f
isinthesky <hijde@naver.com>
2024-07-04 00:13:57 +0900

    test:make-err

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </paz>
+        </pdaz>
         <a
           className="App-link"
           href="https://reactjs.org"
